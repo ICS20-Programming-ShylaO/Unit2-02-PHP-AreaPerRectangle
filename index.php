b45fd8b06a9cd432a96c62fe46783b8f2b5330a4
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-03-09 01:20:32 +0000

    fixed spelling errors

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 	<head>
     <meta charset="utf-8">
-    <meta name="description" content="My first webpage, with JavaScript">
+    <meta name="description" content="My First Math Web Page, Using PHP">
     <meta name="keywords" content="immaculata, ics2o">
-    <meta name="author" content="Ms Raffin">
+    <meta name="author" content="Shyla Oommen">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Area & Perimeter of Rectangle in PHP</title>
 	</head>
@@ -22,7 +22,7 @@
       echo "<p>Radius = $radius cm</p>" ?>
 
     <?php
-      echo "<p>The area of a circle with a radius of $radius cm = " . ($length ** 2 * M_PI) . "cm<sup>2</sup></p>";
+      echo "<p>The area of a circle with a radius of $radius cm = " . ($radius ** 2 * M_PI) . "cm<sup>2</sup></p>";
       ?>
       
 	</body>
